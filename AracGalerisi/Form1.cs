04ed750ba83967d47d156ce2cd585bdb200642a8
cansucreates <caysagdic@gmail.com>
2024-03-31 00:23:37 +0300

    Assigment

---
+++ AracGalerisi/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AracGalerisi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string kullaniciAdi = "Cansu";
+            string sifre = "1234";
+
+            if(textBox1.Text == kullaniciAdi && textBox2.Text == sifre)
+            {
+                MusteriBilgi musteriBilgi = new MusteriBilgi();
+                musteriBilgi.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Kullanıcı adı veya şifre hatalı!");
+            }
+        }
+    }
+}
---
+++ AracGalerisi/MusteriBilgi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AracGalerisi
+{
+    public partial class MusteriBilgi : Form
+    {
+        public MusteriBilgi()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AracSatis aracSatis = new AracSatis();
+
+            aracSatis.listBox1.Items.Add("Müşteri adı : " + textBox1.Text);
+            aracSatis.listBox1.Items.Add("Müşteri soyadı : " + textBox2.Text);
+            aracSatis.listBox1.Items.Add("Telefon no : " + maskedTextBox1.Text);
+            aracSatis.listBox1.Items.Add("Adres : " + textBox3.Text);
+  
+            aracSatis.Show();
+            this.Hide();
+        }
+    }
+}
